9060c3449ea02c06eedf1fb23e15a49827a15b75
Murray-414 <ericsen72k@gmail.com>
2025-11-06 20:10:04 +0300

    Update Motion Activated Security Light codes.ino

--- Motion Activated Security Light codes.ino
+++ Motion Activated Security Light codes.ino
@@ -1,7 +1,7 @@
 // Motion Activated Security Light
 
 int pirPin = 2;       // PIR sensor output
-int ledPin = 13;      // LED pin
+int ledPin = 13;     // Led Pin.
 int pirState = LOW;
 int val = 0;
 
